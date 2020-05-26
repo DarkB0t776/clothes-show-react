c87bc1c9f95ebe5c256fd0866ce58322a3bb30d5
Dima <dima23790@gmail.com>
2020-05-26 14:28:23 +0300

    added collection preview and item components

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+import { SHOP_DATA } from '../../data/data';
+
+import CollectionPreview from '../../components/preview-collection/collection-preview';
+
+const ShopPage = () => {
+  const [collections, setCollections] = useState(SHOP_DATA);
+
+  return (
+    <div className="shop-page">
+      {
+        collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview
+            key={id}
+            {...otherCollectionProps}
+          />
+        ))
+      }
+    </div>
+  );
+}
+
+export default ShopPage;
