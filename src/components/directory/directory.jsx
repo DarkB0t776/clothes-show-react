796c185c1d09d1f145ad24be52571fabaa63d6d2
Dima <dima23790@gmail.com>
2020-05-26 13:20:02 +0300

    added withRouter to menu item component

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -11,12 +11,13 @@ const Directory = () => {
   return (
     <div className="directory-menu">
       {
-        sections.map(({ title, imageUrl, id, size }) => (
+        sections.map(({ title, imageUrl, id, size, linkUrl }) => (
           <MenuItem
             key={id}
             title={title}
             imageUrl={imageUrl}
             size={size}
+            linkUrl={linkUrl}
           />
         ))
       }
